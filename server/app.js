dee98a768bef2f7607051becd78d47f5046ec7a9
Jan Co Chiong <jmscochiong@gmail.com>
2019-05-23 09:01:13 -0400

    Add apollo-provider and allow cors

--- server/app.js
+++ server/app.js
@@ -3,8 +3,12 @@ const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema.js');
 const mongoose = require('mongoose');
 const CONSTANT = require('./constant');
+const cors = require('cors');
 
 const app = express();
+
+// allow cross-origin requests
+app.use(cors());
 
 mongoose.connect(`mongodb://${CONSTANT.MONGODB_USERNAME}:${CONSTANT.MONGODB_PASSWORD}@ds147011.mlab.com:47011/heroku_j2m9j51t`,{ useNewUrlParser: true });
 mongoose.connection.once('open', () => {
